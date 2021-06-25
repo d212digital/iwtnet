a6c5509aac6af5d025b812921138c7ddc0e463e1
David Adamson <david@212digital.co.uk>
2021-06-25 14:21:56 +0100

    adding product repo

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -7,6 +7,7 @@ using Core.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Core.Interfaces;
 
 namespace API.Controllers
 {
@@ -15,17 +16,19 @@ namespace API.Controllers
 
     public class ProductsController : ControllerBase
     {
-        private readonly StoreContext _context;
-        public ProductsController(StoreContext context)
+        private readonly IProductRepository _repo;
+
+        public ProductsController(IProductRepository repo)
         {
-            _context = context;
+            _repo = repo;
+
         }
 
         [HttpGet]
 
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products = await _context.Products.ToListAsync();
+            var products = await _repo.GetProductsAsync();
 
             return Ok(products);
         }
@@ -34,9 +37,22 @@ namespace API.Controllers
 
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return await _context.Products.FindAsync(id);
+            return await _repo.GetProductByIdAsync(id);
         }
 
+        [HttpGet("brands")]
+
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
+        {
+            return Ok (await _repo.GetProductBrandsAsync());
+        } 
+
+        [HttpGet("types")]
+
+        public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
+        {
+            return Ok (await _repo.GetProductTypesAsync());
+        } 
     }
 
 }
