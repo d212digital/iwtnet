9f85000f2dd1417d064e550b1769f7748a6f3bd4
David Adamson <david@212digital.co.uk>
2022-04-26 14:59:13 +0100

    adding generic repo

--- Infrastructure/Data/GenericRepository.cs
+++ Infrastructure/Data/GenericRepository.cs
@@ -1,7 +1,9 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Core.Entities;
 using Core.Interfaces;
+using Core.Specifications;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data
@@ -24,6 +26,21 @@ namespace Infrastructure.Data
         public async Task<IReadOnlyList<T>> ListAllAsync()
         {
            return await _context.Set<T>().ToListAsync();
+        }
+
+        public Task<T> GetEntityWithSpec(ISpecification<T> spec)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private IQueryable<T> ApplySpecification(ISpecification<T> spec)
+        {
+            return SpecificationEvaluator<T>.GetQuery(_context.Set<T>().AsQueryable(), spec);
         }
     }
 }
---
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Specifications;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class SpecificationEvaluator<TEntity> where TEntity : BaseEntity
+    {
+      public static IQueryable<TEntity> GetQuery(IQueryable<TEntity> inputQuery,
+        ISpecification<TEntity> spec)
+      {
+        var query = inputQuery;
+
+        // modify the IQueryable using the specification's criteria expression
+        if (spec.Criteria != null)
+        {
+          query = query.Where(spec.Criteria);
+        }
+
+        // Includes all expression-based includes
+        query = spec.Includes.Aggregate(query,
+          (current, include) => current.Include(include));
+
+        return query;
+      }
+    }
+}
