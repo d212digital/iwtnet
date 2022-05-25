1df0e2695775992d8b1f8f6e840def11f00dcace
David Adamson <david@212digital.co.uk>
2022-05-25 15:44:39 +0100

    adding error handling to the API

--- API/Startup.cs
+++ API/Startup.cs
@@ -67,11 +67,12 @@ namespace API
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseMiddleware<ExceptionMiddleware>();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             
             if (env.IsDevelopment())
             {
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+                
             }
 
             app.UseStatusCodePagesWithReExecute("/errors/{0}");
