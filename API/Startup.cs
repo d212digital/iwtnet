ce7e706655474bd29da4df10e6d79d1946ce2a95
David Adamson <david@212digital.co.uk>
2022-05-26 05:56:38 +0100

    adding service extensions to API

--- API/Startup.cs
+++ API/Startup.cs
@@ -17,6 +17,7 @@ using Core.Interfaces;
 using API.Helpers;
 using API.Middleware;
 using API.Errors;
+using API.Extensions;
 
 namespace API
 {
@@ -32,30 +33,13 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
-            services.AddScoped<IProductRepository, ProductRepository>();
+            
             services.AddAutoMapper(typeof(MappingProfiles));
             services.AddControllers();
             services.AddDbContext<StoreContext>(x =>
                 x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
-
-            services.Configure<ApiBehaviorOptions>(options =>
-            {
-                options.InvalidModelStateResponseFactory = actionContext =>
-                {
-                    var errors = actionContext.ModelState
-                        .Where(e => e.Value.Errors.Count > 0)
-                        .SelectMany(x => x.Value.Errors)
-                        .Select(x => x.ErrorMessage).ToArray();
-
-                    var errorResponse = new ApiValidationErrorResponse
-                    {
-                        Errors = errors
-                    };
-
-                    return new BadRequestObjectResult(errorResponse);
-                };
-            });
+            
+            services.AddApplicationServices();
             
             services.AddSwaggerGen(c =>
             {
